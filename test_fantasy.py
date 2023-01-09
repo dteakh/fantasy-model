8aa1329294c0a876b2b6445d12f01ae7f7c4f04f
Tikhonov Daniil <97369539+dteakh@users.noreply.github.com>
2023-01-10 00:00:06 +0300

    tests changed

--- test_fantasy.py
+++ test_fantasy.py
@@ -1,97 +1,66 @@
 import unittest
 import datetime as dt
-from fantasy import get_event_info, get_events_links, get_stats, EventFilter, OpponentFilter, Event
+from fantasy import Event, Player, FantasyError, PlayerStats, EventFilter, RankFilter
 
-# Tests for 08-01-2023.
+# Tests for 09-01-2023.
 
 
 class TestFantasy(unittest.TestCase):
 
-    def test_event_info(self):
-        t1 = get_event_info(
-            "https://www.hltv.org/events/6810/iem-katowice-2023-play-in")
-        self.assertEqual(len(t1.players), 77)
-        self.assertEqual(t1.players["11630"], "jt")
-        self.assertEqual(t1.players["9102"], "sico")
-        t2 = get_event_info(
-            "https://www.hltv.org/events/5605/blast-premier-fall-groups-2021")
-        self.assertEqual(len(t2.players), 60)
-        self.assertEqual(t2.players["7998"], "s1mple")
-        self.assertRaises(KeyError, t2.players.__getitem__, "19230")
-        self.assertRaises(KeyError, t2.players.__getitem__, "666")
-        self.assertEqual(t2.players["9766"], "hampus")
-        t3 = get_event_info(
-            "https://www.hltv.org/events/6586/iem-rio-major-2022")
-        self.assertEqual(len(t3.players), 80)
-        t4 = get_event_info(
-            "https://www.hltv.org/events/6964/esl-pro-league-season-17-conference-oceania"
-        )
-        self.assertEqual(len(t4.players), 10)
-        t5 = get_event_info(
-            "https://www.hltv.org/events/6863/esl-pro-league-season-18")
-        self.assertEqual(len(t5.players), 73)
-        self.assertEqual(t5.players["21708"], "npl")
-        self.assertRaises(KeyError, t5.players.__getitem__, "12731")
+    def test_event_players(self):
+        t1 = Event(6810).get_players()
+        self.assertEqual(len(t1), 79)
+        self.assertEqual(t1[45].key, 11630)
+        self.assertEqual(t1[60], Player(key=9102, name="sico"))
+        t2 = Event(5605).get_players()
+        self.assertEqual(len(t2), 60)
+        self.assertEqual(t2[0].key, 7998)
+        self.assertFalse(Player(key=19230, name="m0nesy") in t2)
+        self.assertFalse(Player(key=666, name="lavega") in t2)
+        self.assertEqual(t2[27], Player(key=9766, name="hampus"))
+        t3 = Event(6586).get_players()
+        self.assertEqual(len(t3), 80)
+        t4 = Event(6964).get_players()
+        self.assertEqual(len(t4), 10)
+        t5 = Event(6863).get_players()
+        self.assertEqual(len(t5), 74)
+        self.assertEqual(t5[34].key, 21708)
+        self.assertFalse(Player(key=12731, name="sdy") in t5)
 
-    def test_events_filters(self):
-        players = get_event_info(
-            "https://www.hltv.org/events/6970/blast-premier-spring-groups-2023"
-        ).players
-        t1 = get_events_links("13915", players["13915"], dt.date(2022, 8, 30),
-                              dt.date.today(), EventFilter.ALL)
+    def test_events_filter(self):
+        players = Event(6970).get_players()
+        t1 = players[14].get_events(dt.date(2022, 8, 30), dt.date.today(), EventFilter.ALL)
         self.assertEqual(len(t1), 6)
-        t2 = get_events_links("13915", players["13915"], dt.date(2022, 8, 30),
-                              dt.date.today(), EventFilter.LAN)
+        t2 = players[14].get_events(dt.date(2022, 8, 30), dt.date.today(), EventFilter.LAN)
         self.assertEqual(len(t2), 5)
-        t3 = get_events_links("13915", players["13915"], dt.date(2022, 8, 30),
-                              dt.date.today(), EventFilter.BIG)
+        t3 = players[14].get_events(dt.date(2022, 8, 30), dt.date.today(), EventFilter.BIG)
         self.assertEqual(len(t3), 4)
-        t4 = get_events_links("13915", players["13915"], dt.date(2022, 8, 30),
-                              dt.date.today(), EventFilter.MAJORS)
+        t4 = players[14].get_events(dt.date(2022, 8, 30), dt.date.today(), EventFilter.MAJORS)
         self.assertEqual(len(t4), 1)
-        t5 = get_events_links("8520", players["8520"], dt.date(2022, 3, 10),
-                              dt.date.today(), EventFilter.ALL)
+        t5 = players[11].get_events(dt.date(2022, 3, 10), dt.date.today(), EventFilter.ALL)
         self.assertEqual(len(t5), 15)
-        t6 = get_events_links("8520", players["8520"], dt.date(2022, 3, 10),
-                              dt.date(2022, 11, 4), EventFilter.ALL)
+        t6 = players[11].get_events(dt.date(2022, 3, 10), dt.date(2022, 11, 4), EventFilter.ALL)
         self.assertEqual(len(t6), 12)
-        t7 = get_events_links("7998", players["7998"], dt.date.today(),
-                              dt.date.today(), EventFilter.LAN)
+        t7 = players[20].get_events(dt.date.today(), dt.date.today(), EventFilter.LAN)
         self.assertEqual(len(t7), 0)
 
     def test_get_stats(self):
-        plrs = get_event_info(
-            "https://www.hltv.org/events/5728/dreamhack-masters-spring-2021"
-        ).players
-        pl_id = "16920"
-        events = get_events_links(pl_id, plrs[pl_id], dt.date(2022, 1, 9),
-                                  dt.date.today(), EventFilter.ALL)
-        event = events[0].split('=')[-1]
-        st = get_stats(pl_id, plrs[pl_id], event, OpponentFilter.ALL)
+        players = Event(5728).get_players()
+        st = players[3].get_event_stats(Event(6586), RankFilter.ALL)
         self.assertEqual(st.rating, 1.35)
         self.assertEqual(st.adr, 81.2)
-        event = events[1].split('=')[-1]
-        st = get_stats(pl_id, plrs[pl_id], event, OpponentFilter.TOP10)
+        st = players[3].get_event_stats(Event(6588), RankFilter.TOP10)
         self.assertEqual(st.impact, 1.87)
         self.assertEqual(st.dpr, 0.53)
-        event = events[-1].split('=')[-1]
-        st = get_stats(pl_id, plrs[pl_id], event, OpponentFilter.TOP5)
-        self.assertEqual(st, ValueError)
-        pl_id = "17306"
-        events = get_events_links(pl_id, plrs[pl_id], dt.date(2022, 7, 9),
-                                  dt.date.today(), EventFilter.BIG)
-        event = events[0].split('=')[-1]
-        st = get_stats(pl_id, plrs[pl_id], event, OpponentFilter.TOP20)
+        st = players[3].get_event_stats(Event(6334), RankFilter.TOP5)
+        self.assertEqual(type(st), ValueError)
+        st = players[48].get_event_stats(Event(6349), RankFilter.TOP20)
         self.assertEqual(st.rating, 1.19)
         self.assertEqual(st.adr, 71.0)
-        st = get_stats(pl_id, plrs[pl_id], event, OpponentFilter.TOP10)
-        self.assertEqual(st, ValueError)
-        pl_id = "15369"
-        events = get_events_links(pl_id, plrs[pl_id], dt.date(2022, 10, 9),
-                                  dt.date.today(), EventFilter.ALL)
-        event = events[-1].split('=')[-1]
-        st = get_stats(pl_id, plrs[pl_id], event, OpponentFilter.TOP30)
-        self.assertEqual(st, ValueError)
-        st = get_stats(pl_id, plrs[pl_id], event, OpponentFilter.ALL)
+        st = players[48].get_event_stats(Event(6349), RankFilter.TOP10)
+        self.assertEqual(type(st), ValueError)
+        st = players[79].get_event_stats(Event(6828), RankFilter.TOP30)
+        self.assertEqual(type(st), ValueError)
+        st = players[79].get_event_stats(Event(6828), RankFilter.ALL)
         self.assertEqual(st.rating, 0.99)
         self.assertEqual(st.impact, 0.76)
